c3c63414cc2f4e979521c3903ec1ed86440fbb01
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-08-17 14:39:33 +0200

    Physics added.

--- BeastGamesGameTask/Assets/Scripts/Control/PlayerMover.cs
+++ BeastGamesGameTask/Assets/Scripts/Control/PlayerMover.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Control
@@ -6,10 +5,16 @@ namespace Control
     public class PlayerMover : MonoBehaviour
     {
         [SerializeField] private float speed = 12f;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private float groundDistance = 0.4f;
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField] private float gravity = -9.81f;
         
         private CharacterController _controller;
+        private Vector3 _velocity;
         private float HorizontalInput => Input.GetAxis("Horizontal");
         private float VerticalInput => Input.GetAxis("Vertical");
+        private bool IsGrounded => Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         private void Awake()
         {
@@ -19,6 +24,18 @@ namespace Control
         void Update()
         {
             ManageMovement();
+            ManagePhysics();
+        }
+
+        private void ManagePhysics()
+        {
+            if (IsGrounded && _velocity.y < 0f)
+            {
+                _velocity.y = -0.5f;
+            }
+
+            _velocity.y += 0.5f * gravity * Mathf.Pow(Time.deltaTime, 2);
+            _controller.Move(_velocity);
         }
 
         private void ManageMovement()
